6c5f3ca41c48e62af6d4194c57b96cbe1c915ceb
muraronicola <nicola.muraro@studenti.unitn.it>
2025-03-12 09:24:00 +0100

    refactoring main logic

---
+++ execute.sh
@@ -0,0 +1,2 @@
+mpiexec -n 1 ./gca $1 $2 $3 0
+mpiexec -n 2 ./gca $1 $2 $3 0
--- main.c
+++ main.c
@@ -9,20 +9,21 @@
 #include <sys/time.h>
 #include <stdbool.h>
 
-void print_results(double* best_agent, double (*target_function)(double*, int), int dim);
+void print_results(double *best_agent, double (*target_function)(double *, int), int dim);
 
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     MPI_Init(NULL, NULL);
 
     int comm_sz, my_rank;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
-    //srand(time(NULL));
-    srand(10); //Fix the seed for reproducibility (debug only)
+    // srand(time(NULL));
+    srand(10); // Fix the seed for reproducibility (debug only)
 
-    if (argc != 5){
+    if (argc != 5)
+    {
         printf("Usage: %s <dim> <pop_size> <n_iter> <debug>\n", argv[0]);
         return 1;
     }
@@ -32,58 +33,61 @@ int main(int argc, char *argv[]){
     int n_iter = atoi(argv[3]);
     bool debug = atoi(argv[4]);
 
-    int pop_per_proc = (int) pop_size/comm_sz;
-    double* best_agent;
+    int pop_per_proc = (int)pop_size / comm_sz;
+    double *best_agent;
     long seconds, microseconds;
     double elapsed;
 
     struct timeval begin, end;
 
-    
-    if (my_rank == 0){
-        printf("my_rank: %d\n", my_rank);
+    if (comm_sz == 1)
+    {
+        printf("Configuration: dim = %d, pop_size = %d, n_iter = %d\n", dim, pop_size, n_iter);
+        printf("\n");
+
         gettimeofday(&begin, 0);
         best_agent = serial_gca(sphere, -100, 100, dim, pop_size, n_iter, debug);
         gettimeofday(&end, 0);
 
         seconds = end.tv_sec - begin.tv_sec;
         microseconds = end.tv_usec - begin.tv_usec;
-        elapsed = seconds + microseconds*1e-6;
+        elapsed = seconds + microseconds * 1e-6;
 
-        printf("Configuration: dim = %d, pop_size = %d, n_iter = %d\n", dim, pop_size, n_iter);
-        printf("\n");
-    
         printf("----------------------------------------\n");
         printf("Serial GCA\n");
         printf("----------------------------------------\n");
         print_results(best_agent, sphere, dim);
         printf("Time measured: %.3f seconds.\n", elapsed);
         printf("\n\n----------------------------------------\n");
-        printf("Parallel GCA\n");
-        printf("----------------------------------------\n");
     }
+    else
+    {
+        srand(10);
+        gettimeofday(&begin, 0);
+        best_agent = gca(sphere, -100, 100, dim, pop_size, n_iter, my_rank, pop_per_proc, debug);
+        gettimeofday(&end, 0);
 
-    srand(10);
-    gettimeofday(&begin, 0);
-    best_agent = gca(sphere, -100, 100, dim, pop_size, n_iter, my_rank, pop_per_proc, debug);
-    gettimeofday(&end, 0);
-
-    if (my_rank == 0){
-        seconds = end.tv_sec - begin.tv_sec;
-        microseconds = end.tv_usec - begin.tv_usec;
-        elapsed = seconds + microseconds*1e-6;
-        print_results(best_agent, sphere, dim);
-        printf("Time measured: %.3f seconds.\n", elapsed);
+        if (my_rank == 0)
+        {
+            printf("Parallel GCA\n");
+            printf("----------------------------------------\n");
+            seconds = end.tv_sec - begin.tv_sec;
+            microseconds = end.tv_usec - begin.tv_usec;
+            elapsed = seconds + microseconds * 1e-6;
+            print_results(best_agent, sphere, dim);
+            printf("Time measured: %.3f seconds.\n", elapsed);
+        }
     }
 
     MPI_Finalize();
     return 0;
 }
 
-
-void print_results(double* best_agent, double (*target_function)(double*, int), int dim){
+void print_results(double *best_agent, double (*target_function)(double *, int), int dim)
+{
     printf("Best agent: ");
-    for (int i = 0; i < dim; i++){
+    for (int i = 0; i < dim; i++)
+    {
         printf("%f ", best_agent[i]);
     }
     printf("\n");
