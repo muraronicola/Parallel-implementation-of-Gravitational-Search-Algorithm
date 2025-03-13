7cde2d59c57bc73b5d8e40836cc76044075d97dc
muraronicola <nicola.muraro@studenti.unitn.it>
2025-03-13 14:51:01 +0100

    update cluster_test_1

--- scripts/custer_test_1.sh
+++ scripts/custer_test_1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 #PBS -l select=1:ncpus=1:mem=2gb
 # set max execution time
-#PBS -l walltime=0:20:00
+#PBS -l walltime=0:30:00
 # set the queue
 #PBS -q short_cpuQ
 
 module load mpich-3.2
 mpirun.actual -n 1 /home/nicola.muraro/project_hpc/GSA/gca 2 32000 100 0
+
+
+#Reached waaltime dopo 30 minuti, direi che evito questo test
